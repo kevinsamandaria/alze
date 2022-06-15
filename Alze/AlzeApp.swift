60b310ddbdecf1210344da54b78085b40f5f9011
Ridho Saputra <hiputraa@gmail.com>
2022-06-15 14:39:07 +0700

    Add page Add Goal

--- Alze/AlzeApp.swift
+++ Alze/AlzeApp.swift
@@ -12,8 +12,9 @@ struct AlzeApp: App {
 
     var body: some Scene {
         WindowGroup {
-            Login()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            AddGoal()
+//            Login()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
